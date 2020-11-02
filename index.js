73188a46001859394a571355da87cfc7c7c27e3c
Boris Pöhland <boris.poehland@gmail.com>
2020-11-02 13:06:39 +0100

    init

--- index.js
+++ index.js
@@ -1,107 +1,74 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'jest'],
-  extends: [
-    'airbnb/hooks',
-    'plugin:jest/recommended',
-    'prettier',
-    'prettier/react',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  rules: {
-    'prettier/prettier': ['error'],
-    'react/jsx-props-no-spreading': 'off',
-  },
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  settings: { react: { version: 'detect' } },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
+    parser: 'babel-eslint',
+    parserOptions: {
         ecmaFeatures: {
-          jsx: true,
+            jsx: true
         },
-        ecmaVersion: 2018,
+        ecmaVersion: 12,
         sourceType: 'module',
-        project: './tsconfig.json',
-      },
-      plugins: ['react', 'jest', '@typescript-eslint'],
-      extends: [
-        'airbnb-typescript',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
+    },
+    plugins: ['react'],
+    extends: [
+        '@bpoehland/eslint-config-basic',
+        'airbnb',
         'airbnb/hooks',
-        'plugin:jest/recommended',
-        'prettier',
         'prettier/react',
-        'plugin:prettier/recommended',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
-      ],
-      rules: {
-        '@typescript-eslint/typedef': [
-          'error',
-          {
-            arrayDestructuring: true,
-            arrowParameter: true,
-            memberVariableDeclaration: true,
-            objectDestructuring: true,
-            parameter: true,
-            propertyDeclaration: true,
-            variableDeclaration: true,
-            variableDeclarationIgnoreFunction: false,
-          },
-        ],
-        '@typescript-eslint/explicit-function-return-type': [
-          'error',
-          {
-            allowExpressions: false,
-            allowTypedFunctionExpressions: false,
-            allowHigherOrderFunctions: false,
-            allowDirectConstAssertionInArrowFunctions: false,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: false,
-          },
-        ],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            selector: 'typeLike',
-            format: ['PascalCase'],
-            leadingUnderscore: 'allow',
-          },
-        ],
-        'prettier/prettier': ['error'],
+    ],
+    rules: {
         'react/jsx-props-no-spreading': 'off',
-        'no-underscore-dangle': 'off',
-      },
-      env: {
+    },
+    env: {
         browser: true,
-        es6: true,
-        jest: true,
-      },
-      globals: {
+        es2021: true,
+    },
+    globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
-      },
-      settings: { react: { version: 'detect' } },
     },
-  ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                ecmaVersion: 12,
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+            plugins: ['react', '@typescript-eslint'],
+            extends: [
+                '@bpoehland/eslint-config-basic',
+                'airbnb-typescript',
+                'airbnb/hooks',
+                'prettier/react',
+                'plugin:react/recommended',
+                'plugin:react-hooks/recommended',
+            ],
+            rules: {
+                'react/jsx-props-no-spreading': 'off',
+            },
+            env: {
+                browser: true,
+                es2021: true,
+            },
+            globals: {
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+            },
+            settings: {
+                react: {
+                    version: 'detect'
+                }
+            },
+        },
+    ],
 };
